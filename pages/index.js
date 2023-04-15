09ad56edd84707694942b3915dcbe70766291aa9
grzeskid <szymanekgrzegorz@gmail.com>
2023-04-15 21:45:03 +0200

    add new button, reorganizing table

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Baner from "./components/Baner";
-import Footer from "./components/Footer";
-import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
--- pages/table/index.js
+++ pages/table/index.js
@@ -1,27 +1,27 @@
 import { supabase } from "@/lib/supabaseClient";
-import Footer from "../components/Footer";
-import Navigation from "../components/Navigation";
-import { resultsTableHelper } from "../../lib/resultsTableHelper"; 
+import { resultsTableHelper } from "../../lib/resultsTableHelper";
 
 export async function getServerSideProps() {
-  const { data: scores } = await supabase.from('scores').select('*');
-  const { data: players } = await supabase.from('players').select('*');
+  const { data: scores } = await supabase.from("scores").select("*");
+  const { data: players } = await supabase.from("players").select("*");
 
   return {
     props: {
       players: players,
       scores: scores,
-    }
-  }
+    },
+  };
 }
 
 export default function Table(data) {
   const players = data.players;
   const scores = data.scores;
 
-  const table = resultsTableHelper(players, scores).sort((a,b) => a.leaguePoints - b.leaguePoints).reverse()
+  const table = resultsTableHelper(players, scores)
+    .sort((a, b) => a.leaguePoints - b.leaguePoints)
+    .reverse();
 
-  console.log(table)
+  console.log(table);
 
   return (
     <>
@@ -32,30 +32,75 @@ export default function Table(data) {
             <thead>
               <tr>
                 <th className="text-center">Miejsce</th>
-                <th className="text-center">Punkty</th>
                 <th className="text-center">Gracz</th>
-                <th className="text-center">Ilość meczów</th>
-                <th className="text-center">Procent wygranych</th>
-                <th className="text-center">Sety wygrane</th>
-                <th className="text-center">Sety przegrane</th>
-                <th className="text-center">+/-</th>
-                <th className="text-center">Punkty zdobyte</th>
-                <th className="text-center">Ilość przegrane</th>
-                <th className="text-center">+/-</th>
+                <th className="text-center">
+                  Punkty
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  Procent wygranych
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  Ilość meczów
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center border-l-2 border-slate-400">
+                  Sety wygrane
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  Sety przegrane
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  +/-
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center border-l-2 border-slate-400">
+                  Punkty zdobyte
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  Punkty przegrane
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
+                <th className="text-center">
+                  +/-
+                  <div className="flex justify-center w-full py-1">
+                    <kbd className="kbd">sortuj ▼</kbd>
+                  </div>
+                </th>
               </tr>
             </thead>
             <tbody>
               {table.map((player, i) => (
                 <tr key={i}>
                   <th className="text-center">{i + 1}</th>
-                  <th className="text-center">{player.leaguePoints}</th>
                   <th className="text-center">{player.playerName}</th>
+                  <th className="text-center">{player.leaguePoints}</th>
+                  <th className="text-center">{player.wonPercentage} %</th>
                   <th className="text-center">{player.games}</th>
-                  <th className="text-center">{player.wonPercentage}</th>
-                  <th className="text-center">{player.wonSets}</th>
+                  <th className="text-center border-l-2 border-slate-400">{player.wonSets}</th>
                   <th className="text-center">{player.lostSets}</th>
                   <th className="text-center">{player.setsDifference}</th>
-                  <th className="text-center">{player.pointsWon}</th>
+                  <th className="text-center border-l-2 border-slate-400">{player.pointsWon}</th>
                   <th className="text-center">{player.pointsLost}</th>
                   <th className="text-center">{player.pointsDifference}</th>
                 </tr>
